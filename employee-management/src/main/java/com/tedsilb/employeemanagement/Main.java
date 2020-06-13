4e7e2434be96eb48b757907c3eca1dbee2a09aea
tedsilb <26172399+tedsilb@users.noreply.github.com>
2020-06-12 23:27:43 -0700

    add flogger

--- employee-management/src/main/java/com/tedsilb/employeemanagement/Main.java
+++ employee-management/src/main/java/com/tedsilb/employeemanagement/Main.java
@@ -3,10 +3,13 @@ package com.tedsilb.employeemanagement;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.datastore.Datastore;
+import com.google.common.flogger.FluentLogger;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.tedsilb.employeemanagement.Protos.Employee;
+
 import java.io.IOException;
 import java.util.Scanner;
+
 
 public class Main {
   private static final String SERVICE_ACCOUNT_JSON =
@@ -14,12 +17,14 @@ public class Main {
 
   private static final String DATASTORE_PROJECT_ID = "employee-management-279606";
 
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
   public static void main(final String[] args) {
     GoogleCredentials creds;
     try {
       creds = IOUtil.readGoogleCredentialsFromFile(SERVICE_ACCOUNT_JSON);
     } catch (IOException e) {
-      System.out.println(e);
+      logger.atSevere().withCause(e).log("Service account file not found.");
       return;
     }
 
@@ -45,7 +50,7 @@ public class Main {
     try {
       employee = DatastoreUtil.getEmployee(datastore, name);
     } catch (final InvalidProtocolBufferException e) {
-      System.out.println(e);
+      logger.atSevere().withCause(e);
       scan.close();
       return;
     }
